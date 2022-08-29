0492ae795cd003a13d949dc846b282144ad2f15b
dessa995 <dessa995@outlook.com>
2022-08-29 18:37:09 +0200

    homepage update

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,25 +3,8 @@ $(document).ready(function () {
     centerMode: true,
     centerPadding: "60px",
     slidesToShow: 1,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: "40px",
-          slidesToShow: 3,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: "40px",
-          slidesToShow: 1,
-        },
-      },
-    ],
+    autoplay: true,
+    autoplaySpeed: 5000,
+    variableWidth: true,
   });
 });
